6b5f9d3e860d513a0f9732f689273e4ea00e2cd7
datnikon <vandat03@gmail.com>
2025-12-06 04:00:41 +0700

    Update gift-data.js

--- gifs/gift-data.js
+++ gifs/gift-data.js
@@ -4,16 +4,16 @@ const gifts = [
         name: "Cục Sạc Dự Phòng 'A+'",
         date: "Ngày em đạt A+ Triết Học", // Bạn sửa ngày cụ thể nhé
         image: "vention.png", // Thay bằng ảnh sạc dự phòng thật: "images/sac-du-phong.jpg"
-        meaning: "Món quà thưởng nóng cho thành tích siêu đỉnh (A+ môn Triết). Giúp em xua tan vấn đề hết pin mỗi lần đi xa hay ra ngoài.",
-        usage: "Siêu nhỏ gọn,kute phô mai que,dây đi kèm sạc được cho mọi điện thoại iphone/android,dây có thể biến sạc dự phòng thành móc treo túi sách/cặp. Vừa đủ sạc đầy 1 lần cho điện thoại của em. Có thể dùng bộ sạc iPhone của em để sạc!",
-        warning: "⚠️ Cảnh báo quan trọng: Sản phẩm tích hợp AI, khi phát hiện trai lạ thì có tính năng tự động BÙM đó nha! Haha."
+        meaning: "Món quà thưởng nóng cho thành tích siêu đỉnh (A+ môn Triết). Xua tan vấn đề hết pin khi cần thiết.",
+        usage: "Nhỏ gọn,kute phô mai que,dây đi kèm sạc được cho mọi điện thoại. Móc như hình để biến sạc thành móc treo túi sách/cặp. Vừa đủ sạc đầy 1 lần cho điện thoại của em. Dùng bộ sạc iPhone để sạc lại",
+        warning: "⚠️ Cảnh báo quan trọng: Sản phẩm tích hợp AI, khi phát hiện trai lạ thì có tính năng tự động cảnh báo đó nha! Haha."
     },
     {
         name: "Chiếc Loa 'Giai Điệu'",
         date: "Giải vòng chung kết", // Sửa ngày tặng
         image: "go4.png", // Thay bằng ảnh loa thật
-        meaning: "Hi vọng lúc em ở nhà một mình hay đi chơi với Én thì chiếc loa này có thể là người bạn đồng hành thú vị lúc em cần tới âm nhạc 🎧.",
-        usage: "Chống nước, có thể phát nhạc tới 20 tiếng liên tục - đủ để em nghe nhạc trog lúc đợi anh tới, hehe",
-        note: "Biết đâu vào một khoảnh khắc nào đó, nó sẽ phát ra những âm thanh khiến em rung động (như lúc gặp anh vậy đó ^^)."
+        meaning: "Hi vọng chiếc loa nhỏ này có thể là người bạn đồng hành thú vị lúc em ở phòng một mình hay ra ngoài cùng bạn bè 🎧.",
+        usage: "Chống nước, phát nhạc tới 20 tiếng liên tục - đủ để em nghe nhạc trog lúc đợi anh tới, hehe",
+        note: "Biết đâu vào một khoảnh khắc nào đó, với bài hát yêu thích nó sẽ phát ra những âm thanh khiến em rung động (như lúc gặp anh vậy đó ^^)."
     }
 ];
