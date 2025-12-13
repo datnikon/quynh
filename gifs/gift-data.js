d5b9e7f080e926092428fde42c5ebc779753c715
datnikon <vandat03@gmail.com>
2025-12-13 21:51:25 +0700

    Update gift description

--- gifs/gift-data.js
+++ gifs/gift-data.js
@@ -4,16 +4,16 @@ const gifts = [
         name: "Cục Sạc Dự Phòng 'A+'",
         date: "Ngày em đạt A+ Triết Học", // Bạn sửa ngày cụ thể nhé
         image: "vention.png", // Thay bằng ảnh sạc dự phòng thật: "images/sac-du-phong.jpg"
-        meaning: "Món quà thưởng nóng cho thành tích siêu đỉnh (A+ môn Triết). Xua tan vấn đề hết pin khi cần thiết.",
-        usage: "Nhỏ gọn,kute phô mai que,dây đi kèm sạc được cho mọi điện thoại. Móc như hình để biến sạc thành móc treo túi sách/cặp. Vừa đủ sạc đầy 1 lần cho điện thoại của em. Dùng bộ sạc iPhone để sạc lại",
+        meaning: "Món quà thưởng nóng cho thành tích siêu đỉnh (A+ môn Triết). Xua tan vấn đề hết pin cho mọi chuyến đi ^^.",
+        usage: "Một cục sạc bé nhỏ, kute có thẻ sạc mọi điện thoại, làm đồ trang trí. Đủ sạc một lần cho điện thoại của em và có thể dụng bổ sạc Iphone để sạc lại",
         warning: "⚠️ Cảnh báo quan trọng: Sản phẩm tích hợp AI, khi phát hiện trai lạ thì có tính năng tự động cảnh báo đó nha! Haha."
     },
     {
         name: "Chiếc Loa 'Giai Điệu'",
         date: "Giải vòng chung kết", // Sửa ngày tặng
         image: "go4.png", // Thay bằng ảnh loa thật
-        meaning: "Hi vọng chiếc loa nhỏ này có thể là người bạn đồng hành thú vị lúc em ở phòng một mình hay ra ngoài cùng bạn bè 🎧.",
-        usage: "Chống nước, phát nhạc tới 20 tiếng liên tục - đủ để em nghe nhạc trog lúc đợi anh tới, hehe",
-        note: "Biết đâu vào một khoảnh khắc nào đó, với bài hát yêu thích nó sẽ phát ra những âm thanh khiến em rung động (như lúc gặp anh vậy đó ^^)."
+        meaning: "Hi vọng chiếc loa nhỏ này sẽ là người bạn đồng hành thú vị lúc em ở phòng một mình hay ra ngoài cùng bạn bè 🎧.",
+        usage: "Có tính năng chống nước, có thể phát nhạc liên tục tới 20 tiếng - đủ để em nghe nhạc trog lúc đợi anh tới, hehe",
+        note: "Biết đâu vào một khoảnh khắc nào đó, với bài hát yêu thích - chiếc loa này sẽ phát ra những âm thanh khiến em rung động (như lúc gặp anh vậy đó ^^)."
     }
 ];
